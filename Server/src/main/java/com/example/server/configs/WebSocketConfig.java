c7244b51b0c453eaa1665aea919b0b22d9c122fb
Venuk De Soyza <venukdesoyza@gmail.com>
2025-04-03 04:32:37 +0530

    Allowed the vercel url for websocket

--- Server/src/main/java/com/example/server/configs/WebSocketConfig.java
+++ Server/src/main/java/com/example/server/configs/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket").setAllowedOrigins("http://localhost:4200").withSockJS(); // WebSocket endpoint with SockJS fallback
+        registry.addEndpoint("/websocket").setAllowedOrigins("http://localhost:4200", "https://event-ticketing-system-eight.vercel.app").withSockJS(); // WebSocket endpoint with SockJS fallback
     }
 }
